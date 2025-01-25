cbebc3770b4e5e83c1a2dc56d657102c0591c3be
Timothy Cope <kirbycope@gmail.com>
2025-01-25 11:31:05 -0800

    Update export-windows.sh

--- ci/export-windows.sh
+++ ci/export-windows.sh
@@ -6,13 +6,8 @@ preset="Windows Desktop"
 project=$(basename "$(pwd)")
 export_dir="$(pwd)/export"
 
-# Check if the path ends with Godot.app and modify it accordingly
-if [[ "$godot" == */Godot.app ]]; then
-    godot="${godot}/Contents/MacOS/Godot"
-fi
-
 # Print the command before running it
-echo "\"$godot\" --headless --export-release \"$preset\" \"$export_dir/${project}.exe\""
+echo "\"$godot\" --headless --export-release \"$preset\" \"${export_dir}/${project}.exe\""
 
-# Run Godot headless and export the project as a release HTML5 build
+# Run Godot headless and export the project as a release windows build
 "$godot" --headless --export-release "$preset" "$export_dir/${project}.exe"
