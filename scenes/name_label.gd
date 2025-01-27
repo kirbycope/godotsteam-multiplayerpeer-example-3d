613862080c454181d7790933afdf7fb62f2bafbd
Timothy Cope <kirbycope@gmail.com>
2025-01-26 22:03:05 -0800

    persona label

--- scenes/name_label.gd
+++ scenes/name_label.gd
@@ -3,5 +3,6 @@ extends Label3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# Set playername
+	if !is_multiplayer_authority():
+		return
 	text = str(Steam.getPersonaName())
